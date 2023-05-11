2fc82adf04e684cb8660265743194cbfc470a584
DELTA7884 <rahulsharmawins19@gmail.com>
2023-05-11 18:18:22 +0530

    today

---
+++ spaceoddeven.js
@@ -0,0 +1,9 @@
+const names = "MY name is Rahul Sharma";
+
+let split = names.split('');
+for (let i = 0; i < split.length; i++) {
+    if (split[i] === "") {
+    }
+
+    console.log(split[i]);
+}
